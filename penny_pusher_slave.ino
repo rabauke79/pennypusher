de7ce8e2c94403bdf8347e08208f964ec72ad634
rabauke79 <63261875+rabauke79@users.noreply.github.com>
2024-05-24 15:22:14 +0200

    Update penny_pusher_slave.ino

--- penny_pusher_slave.ino
+++ penny_pusher_slave.ino
@@ -1,14 +1,18 @@
 #include <U8g2lib.h>
 
-/* Code for slave devices - they are waiting for getting a signal and showing 
- * grafix / updated per coin lane.
- * Attached is a 128x64 lcd (monochrome)
- * Code runs on an Arduino Nano (ATMega328P)
- * 
- * Initial version 0.1
- * - show default message
- * - wait for an interrupt and display for 2 seconds a different message 
- */
+/* Code for slave devices - they are waiting for getting a signal and showing
+   grafix / updated per coin lane.
+   Attached is a 128x64 lcd (monochrome)
+   Code runs on an Arduino Nano (ATMega328P)
+
+   Initial version 0.1
+   - show default message
+   - wait for an interrupt and display for 2 seconds a different message
+
+   Version 0.2
+   - added counter
+   - "improved" UI
+*/
 
 
 
@@ -17,6 +21,7 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 // Variable zur Verfolgung der Zeit für den Interrupt
 unsigned long lastInterruptTime = 0;
+unsigned int anzahlMuenzen = 0;
 
 void setup() {
   // Initialisiere das Display
@@ -45,8 +50,11 @@ void loop() {
     // Zeige den Text "Warte" auf dem Display an
     u8g2.firstPage();
     do {
-      u8g2.setCursor(0, 10);
-      u8g2.print("Warte");
+      u8g2.setDrawColor(1);
+      u8g2.clearBuffer();
+      drawCoinCounter();
+      drawFrameWaitScreen();
+      drawInsertCoins();
     } while (u8g2.nextPage());
   }
 
@@ -55,4 +63,24 @@ void loop() {
 // Interrupt-Handler
 void handleInterrupt() {
   lastInterruptTime = millis();
+  anzahlMuenzen++;
+}
+
+
+// Funtions to handle the default Screen
+void drawFrameWaitScreen() {
+  u8g2.drawFrame(2, 2, 124, 60);
+  u8g2.drawHLine(2, 45, 124);
+}
+
+void drawCoinCounter() {
+  u8g2.setFont(u8g2_font_ncenB24_tr);
+  u8g2.setCursor(60,32);
+  u8g2.print(anzahlMuenzen);
+}
+
+void drawInsertCoins() {
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.setCursor(6, 57);
+  u8g2.print("Wirf eine Münze ein!");
 }
